eccc37ce7e56158712011c69f83559b939cc48ae
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2023-08-18 13:08:51 +0530

    Initial commit

---
+++ Json-with-Ajax/web/assets/js/CustomerScript.js
@@ -0,0 +1,70 @@
+getAllCustomers();
+
+$("#btnGetAll").click(function () {
+    getAllCustomers();
+});
+
+function getAllCustomers(){
+    $("#tblCustomer").empty();
+    <!--send ajax request to the customer servlet using jQuery-->
+    $.ajax({
+        url: 'customer',
+        dataType:"json",
+        success: function (customers) {
+            for (let i in customers) {
+                let cus = customers[i];
+                let id = cus.id;
+                let name = cus.name;
+                let address = cus.address;
+                let row=`<tr><td>${id}</td><td>${name}</td><td>${address}</td></tr>`;
+                $("#tblCustomer").append(row);
+            }
+        },
+        error:function(error){
+            console.log(error);
+        }
+    });
+}
+
+$("#btnCustomer").click(function (){
+    let formData=$("#customerForm").serialize();
+    $.ajax({
+        url:"customer?option=add",
+        method:"post",
+        data:formData,
+        success:function (res){
+            console.log(res);
+            alert(res.message);
+            getAllCustomers();
+        },
+        error:function (error){
+            console.log(error.responseJSON);
+            alert(error.responseJSON.message);
+        }
+    });
+
+});
+
+$("#btnCusDelete").click(function(){
+    let formData=$("#customerForm").serialize();
+    $.ajax({
+        url:'customer?option=delete',
+        method:'post',
+        data:formData,
+        success:function (resp){
+            getAllCustomers();
+        }
+    });
+});
+
+$("#btnUpdate").click(function (){
+    let formData=$("#customerForm").serialize();
+    $.ajax({
+        url:'customer?option=update',
+        method:'post',
+        data:formData,
+        success:function (resp){
+            getAllCustomers();
+        }
+    });
+});
